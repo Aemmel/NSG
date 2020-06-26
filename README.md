549fcc911b26575c8ba8b24a0fcfa2f0ad64e1e3
Jan Böhmer <jan.h.boehmer@gmx.de>
2020-06-26 15:00:14 +0200

    Added basic CMake build system.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NSG
+
+## Build
+Run `cmake .` to generate a Makefile, run `make` to create an executable.
---
+++ main.c
@@ -0,0 +1,11 @@
+//
+// Created by janhb on 26.06.2020.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Test\n");
+    return 0;
+}
