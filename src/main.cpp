61c355523494e31b1951730fe3fe9089a5a57d08
Emil Donkersloot <emil.donkersloot@online.de>
2020-06-26 18:30:17 +0200

    Implemented Options class to read in set values

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,14 @@
 //
 
 #include <iostream>
+#include <fstream>
+
+#include "Options.hpp"
 
 int main()
 {
+    Options Op = Options("data/options.json");
+
     std::cout << "jan stinkt" << 6 << std::endl;
     return 0;
 }
